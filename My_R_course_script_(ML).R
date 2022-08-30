aaec14dcf8d31eab5fe928699209b8cb8fcdec15
Vasily S. Romanov <47154542+VSRomanov@users.noreply.github.com>
2022-08-30 23:15:07 +0200

    Add files via upload

--- My_R_course_script_(ML).R
+++ My_R_course_script_(ML).R
@@ -6,7 +6,7 @@
 #---------------------------------------------------------------
 
 
-rm(list=ls()) # cleaning all the variables in the Environment
+rm(list = ls()) # cleaning all the variables in the Environment
 
 
 
@@ -41,13 +41,13 @@ iris$ID <- as.character(rownames(iris))
 library(caret)
 
 # plot a boxplot and a density histogram of the iris data for each species
-scales <- list(x=list(relation="free"), y=list(relation="free"))
+scales <- list(x = list(relation = "free"), y = list(relation = "free"))
 png(filename = "featurePlot_Box.png", width = 15, height = 10, units = "cm", res = 300)
-featurePlot(x=iris[,1:4], y=iris$Species, plot = "box", scales = scales)
+featurePlot(x = iris[,1:4], y = iris$Species, plot = "box", scales = scales)
 dev.off()
 png(filename = "featurePlot_Density.png", width = 15, height = 15, units = "cm", res = 300)
-featurePlot(x=iris[,1:4], y=iris$Species, plot = "density", scales=scales,
-            auto.key=list(columns=3))
+featurePlot(x = iris[,1:4], y = iris$Species, plot = "density", scales = scales,
+            auto.key = list(columns = 3))
 dev.off()
 
 
@@ -55,7 +55,7 @@ dev.off()
 # In order to test and see how good our classification performs, we will mask the species labels in 30% of the data.
 # Split the dataset 70/30
 set.seed(9)
-indices <- createDataPartition(iris$Species, p=0.7, list = F)
+indices <- createDataPartition(iris$Species, p = 0.7, list = F)
 # 70% of the data with class labels
 irisWithClassLabel <- iris[indices,]
 dim(irisWithClassLabel)
@@ -99,17 +99,20 @@ head(df_out)
 # assign species label
 df_out$Species <- as.character(data[,5])
 # plot the PCA results
-p1 <- ggplot(df_out, aes(x=PC1, y=PC2, color=Species, label=rownames(df_out))) +
-  geom_point() + geom_text(aes(label=rownames(df_out)), hjust=0, vjust=0) +
+p1 <- ggplot(df_out, aes(x = PC1, y = PC2, color = Species, label = rownames(df_out))) +
+  geom_point() + 
+  geom_text(aes(label = rownames(df_out)), hjust=0, vjust=0) +
   theme_bw()
-p2 <- ggplot(df_out, aes(x=PC1, y=PC3, color=Species, label=rownames(df_out))) +
-  geom_point() +geom_text(aes(label=rownames(df_out)), hjust=0, vjust=0) +
+p2 <- ggplot(df_out, aes(x = PC1, y = PC3, color = Species, label = rownames(df_out))) +
+  geom_point() +
+  geom_text(aes(label = rownames(df_out)), hjust = 0, vjust = 0) +
   theme_bw()
-p3 <- ggplot(df_out, aes(x=PC2, y=PC3, color=Species, label=rownames(df_out))) +
-  geom_point() +geom_text(aes(label=rownames(df_out)), hjust=0, vjust=0) +
+p3 <- ggplot(df_out, aes(x = PC2, y = PC3, color = Species, label = rownames(df_out))) +
+  geom_point() +
+  geom_text(aes(label = rownames(df_out)), hjust = 0, vjust = 0) +
   theme_bw()
 # combine the three plots to one
-pFin <- grid.arrange(p1,p2,p3, ncol=2)
+pFin <- grid.arrange(p1, p2, p3, ncol = 2)
 # save PCA plot as .png file
 ggsave(pFin, filename = "PCA.png", device = "png", dpi = 600, width = 30,
        height = 30, units = "cm")
@@ -119,18 +122,18 @@ ggsave(pFin, filename = "PCA.png", device = "png", dpi = 600, width = 30,
 sampleTree <- hclust(dist(data[,1:4]), method = "mcquitty")
 library(ggdendro)
 # First generate a simple tree
-dendr <- ggdendrogram(sampleTree, labels = F, xlab=F)
+dendr <- ggdendrogram(sampleTree, labels = F, xlab = F)
 # extract the data from the 'hclust' object
 ddata_x <- dendro_data(sampleTree)
 labs <- label(ddata_x)
 # get each ID in the tree
 ID <- as.numeric(as.character(labs$label))
 # find position of each ID in the original data
-Pos <- sapply(as.character(ID), function(x)which(x==as.character(data$ID)))
+Pos <- sapply(as.character(ID), function(x) which(x == as.character(data$ID)))
 
 # assign for each sample the corresponding iris species as color
-Dendro <- dendr + geom_text(data = label(ddata_x), aes(label=label, x=x, y=0,
-                                                       color=data$Species[Pos], angle = 45, hjust=1))
+Dendro <- dendr + geom_text(data = label(ddata_x), aes(label = label, x = x, y = 0,
+                                                       color = data$Species[Pos], angle = 45, hjust = 1))
 # save as .png file
 ggsave(Dendro, filename = "Dendo.png", device = "png", dpi = 600, width = 40,
        height = 20, units = "cm")
@@ -146,7 +149,7 @@ iris$Species <- factor(iris$Species)
 # in order to test how good our classification performs, we will split the data
 # in 70% and 30%, train with 70% of the data and test with the other 30%
 set.seed(9)
-training_indices <- createDataPartition(iris$Species, p=0.7, list = F)
+training_indices <- createDataPartition(iris$Species, p = 0.7, list = F)
 # select 70% of the data to train the models
 irisTrain <- iris[training_indices,]
 # use the remaining 30% of the data for model testing
@@ -164,15 +167,15 @@ metric <- "Balanced_Accuracy"
 
 # LDA
 set.seed(7)
-fit.lda <- train(x=irisTrain[,1:4], y=irisTrain[,5], method = "lda", metric = metric,
+fit.lda <- train(x = irisTrain[,1:4], y = irisTrain[,5], method = "lda", metric = metric,
                  trControl = control, tuneLength = 10, preProcess = c("center", "scale"))
 # k-NN
 set.seed(7)
-fit.knn <- train(x=irisTrain[,1:4], y=irisTrain[,5], method = "knn", metric = metric,
+fit.knn <- train(x = irisTrain[,1:4], y = irisTrain[,5], method = "knn", metric = metric,
                  trControl = control, tuneLength = 10, preProcess = c("center", "scale"))
 # Decision Tree
 set.seed(7)
-fit.cart <- train(x=irisTrain[,1:4], y=irisTrain[,5], method = "rpart", metric = metric,
+fit.cart <- train(x = irisTrain[,1:4], y = irisTrain[,5], method = "rpart", metric = metric,
                   trControl = control, tuneLength = 10, preProcess = c("center","scale"))
 # plot the best decision tree
 library(rpart.plot)
@@ -180,35 +183,35 @@ rpart.plot(fit.cart$finalModel, type = 5)
 
 # SVM Linear
 set.seed(7)
-fit.svm_Lin <- train(x=irisTrain[,1:4], y=irisTrain[,5], method = "svmLinear", metric = metric,
+fit.svm_Lin <- train(x = irisTrain[,1:4], y = irisTrain[,5], method = "svmLinear", metric = metric,
                      trControl = control, tuneLength = 10, preProcess = c("center", "scale"))
 
 # SVM Radial Basis Function
 set.seed(7)
-fit.svm_Rad <- train(x=irisTrain[,1:4], y=irisTrain[,5], method = "svmRadial", metric = metric,
+fit.svm_Rad <- train(x = irisTrain[,1:4], y = irisTrain[,5], method = "svmRadial", metric = metric,
                      trControl = control, tuneLength = 10, preProcess = c("center", "scale"))
 
 # Random Forest
 set.seed(7)
-fit.rf <- train(x=irisTrain[,1:4], y=irisTrain[,5], method = "rf", metric = metric,
+fit.rf <- train(x = irisTrain[,1:4], y = irisTrain[,5], method = "rf", metric = metric,
                 trControl = control, tuneLength = 10, preProcess = c("center", "scale"))
 
 # Neural Networks
 set.seed(7)
-fit.nnet <- train(x=irisTrain[,1:4], y=irisTrain[,5], method = "nnet", metric = metric,
+fit.nnet <- train(x = irisTrain[,1:4], y = irisTrain[,5], method = "nnet", metric = metric,
                   trControl = control, tuneLength = 10, preProcess = c("center", "scale"))
 
 
 
 ## Summarize ML performance
-results <- resamples(list(lda=fit.lda, knn=fit.knn, svm_Lin=fit.svm_Lin, svm_Rad=fit.svm_Rad, rf=fit.rf,
-                          cart=fit.cart, nnet=fit.nnet))
+results <- resamples(list(lda = fit.lda, knn = fit.knn, svm_Lin = fit.svm_Lin, svm_Rad = fit.svm_Rad, rf = fit.rf,
+                          cart = fit.cart, nnet = fit.nnet))
 summary(results)
 
 # plot comparison of models
-scales <- list(x=list(relation="free"), y=list(relation="free"))
+scales <- list(x = list(relation = "free"), y = list(relation = "free"))
 png(filename = "ML_performance.png", width = 20, height = 20, units = "cm", res = 300)
-dotplot(results, scales=scales, par.strip.text=list(cex=0.76), par.settings = list(par.xlab.text = list(cex = 0)))
+dotplot(results, scales = scales, par.strip.text = list(cex = 0.76), par.settings = list(par.xlab.text = list(cex = 0)))
 dev.off()
 # what is the best model in terms of Balanced Accuracy"?
 
@@ -229,7 +232,7 @@ confusionMatrix(predictions, irisTest$Species)
 gbmImp <- varImp(fit.lda, scale = T)
 png(filename = "Iris_ML_FeatImp.png", width = 10, height = 10, units = "cm",
     res = 300)
-plot(gbmImp, main="Iris Variable Importance (LDA)", top=4)
+plot(gbmImp, main = "Iris Variable Importance (LDA)", top = 4)
 dev.off()
 
 
@@ -259,18 +262,18 @@ head(GermanCredit, 20)
 GermanCredit$ID <- as.character(rownames(GermanCredit))
 
 # plot a boxplot and a density histogram of the GermanCredit data for each species
-scales <- list(x=list(relation="free"), y=list(relation="free"))
+scales <- list(x = list(relation = "free"), y = list(relation = "free"))
 png(filename = "featurePlot_Box_GermanCredit.png", width = 15, height = 10, units = "cm", res = 300)
-featurePlot(x=GermanCredit[,c("Duration", "Amount", "Age", "CheckingAccountStatus.none",
+featurePlot(x = GermanCredit[,c("Duration", "Amount", "Age", "CheckingAccountStatus.none",
                               "CheckingAccountStatus.0.to.200",
-                              "CheckingAccountStatus.lt.0")], y=GermanCredit$Class, plot = "box", scales = scales,
-            par.strip.text=list(cex=0.6))
+                              "CheckingAccountStatus.lt.0")], y = GermanCredit$Class, plot = "box", scales = scales,
+            par.strip.text = list(cex = 0.6))
 dev.off()
 png(filename = "featurePlot_Density_GermanCredit.png", width = 15, height = 15, units = "cm", res = 300)
-featurePlot(x=GermanCredit[,c("Duration", "Amount", "Age", "CheckingAccountStatus.none",
+featurePlot(x = GermanCredit[,c("Duration", "Amount", "Age", "CheckingAccountStatus.none",
                               "CheckingAccountStatus.0.to.200",
-                              "CheckingAccountStatus.lt.0")], y=GermanCredit$Class, plot = "density", scales=scales,
-            par.strip.text=list(cex=0.6), auto.key=list(columns=2))
+                              "CheckingAccountStatus.lt.0")], y = GermanCredit$Class, plot = "density", scales = scales,
+            par.strip.text = list(cex = 0.6), auto.key = list(columns = 2))
 dev.off()
 
 
@@ -280,7 +283,7 @@ dev.off()
 # in order to test how good our classification performs, we will split the data
 # in 70% and 30%, train with 70% of the data and test with the other 30%
 set.seed(9)
-training_indices_GC <- createDataPartition(GermanCredit$Class, p=0.7, list = F)
+training_indices_GC <- createDataPartition(GermanCredit$Class, p = 0.7, list = F)
 
 # select 70% of the data to train the models
 GerCredTrain <- GermanCredit[training_indices_GC,]
@@ -300,7 +303,7 @@ selected_features <- all_features[! all_features %in% nz_var]
 ## Removing highly correlated features (Pearson coefficient > 0.7)
 cat("Removing highly correlated features (Pearson coef. > 0.7)\n")
 descrCor <- cor(GerCredTrain[, selected_features], method = "pearson")
-highlyCorDescr <- caret::findCorrelation(descrCor, cutoff = .7, exact = TRUE, names = TRUE)
+highlyCorDescr <- caret::findCorrelation(descrCor, cutoff = 0.7, exact = TRUE, names = TRUE)
 selected_features <- selected_features[! selected_features %in% highlyCorDescr]
 
 
@@ -341,16 +344,16 @@ metric <- "Balanced_Accuracy"
 
 # LDA
 set.seed(7)
-fit.lda.GC <- train(x=GerCredTrain[,selected_features], y=GerCredTrain$Class, method = "lda", metric = metric,
+fit.lda.GC <- train(x = GerCredTrain[,selected_features], y = GerCredTrain$Class, method = "lda", metric = metric,
                     trControl = control.GC, tuneLength = 10, preProcess = c("center", "scale"))
 # k-NN
 set.seed(7)
-fit.knn.GC <- train(x=GerCredTrain[,selected_features], y=GerCredTrain$Class, method = "knn", metric = metric,
+fit.knn.GC <- train(x = GerCredTrain[,selected_features], y = GerCredTrain$Class, method = "knn", metric = metric,
                     trControl = control.GC, tuneLength = 10, preProcess = c("center", "scale"))
 # Decision Tree
 # a very nice feature of decisions tress is that the decisions of the machine can be followed up easily
 set.seed(7)
-fit.cart.GC <- train(x=GerCredTrain[,selected_features], y=GerCredTrain$Class, method = "rpart", metric = metric,
+fit.cart.GC <- train(x = GerCredTrain[,selected_features], y = GerCredTrain$Class, method = "rpart", metric = metric,
                      trControl = control.GC, tuneLength = 10, preProcess = c("center","scale"))
 # plot the best decision tree
 library(rpart.plot)
@@ -358,36 +361,36 @@ rpart.plot(fit.cart.GC$finalModel, type = 5)
 
 # SVM Linear
 set.seed(7)
-fit.svm_Lin.GC <- train(x=GerCredTrain[,selected_features], y=GerCredTrain$Class, method = "svmLinear",
+fit.svm_Lin.GC <- train(x = GerCredTrain[,selected_features], y = GerCredTrain$Class, method = "svmLinear",
                         metric = metric, trControl = control.GC, tuneLength = 10, preProcess = c("center", "scale"))
 
 # SVM Radial Basis Function
 set.seed(7)
-fit.svm_Rad.GC <- train(x=GerCredTrain[,selected_features], y=GerCredTrain$Class, method = "svmRadial",
+fit.svm_Rad.GC <- train(x = GerCredTrain[,selected_features], y = GerCredTrain$Class, method = "svmRadial",
                         metric = metric, trControl = control.GC, tuneLength = 10, preProcess = c("center", "scale"))
 
 # Random Forest
 set.seed(7)
-fit.rf.GC <- train(x=GerCredTrain[,selected_features], y=GerCredTrain$Class, method = "rf", metric = metric,
+fit.rf.GC <- train(x = GerCredTrain[,selected_features], y = GerCredTrain$Class, method = "rf", metric = metric,
                    trControl = control.GC, tuneLength = 10, preProcess = c("center", "scale"))
 
 # Neural Networks
 set.seed(7)
-fit.nnet.GC <- train(x=GerCredTrain[,selected_features], y=GerCredTrain$Class, method = "nnet", metric = metric,
+fit.nnet.GC <- train(x = GerCredTrain[,selected_features], y = GerCredTrain$Class, method = "nnet", metric = metric,
                      trControl = control.GC, tuneLength = 10, preProcess = c("center", "scale"))
 
 
 
 ## summarize ML performance
-results.GC <- resamples(list(lda=fit.lda.GC, knn=fit.knn.GC, svm_Lin=fit.svm_Lin.GC, svm_Rad=fit.svm_Rad.GC,
-                             rf=fit.rf.GC, cart=fit.cart.GC, nnet=fit.nnet.GC))
+results.GC <- resamples(list(lda = fit.lda.GC, knn = fit.knn.GC, svm_Lin = fit.svm_Lin.GC, svm_Rad = fit.svm_Rad.GC,
+                             rf = fit.rf.GC, cart = fit.cart.GC, nnet = fit.nnet.GC))
 summary(results.GC)
 
 
 # plot comparison of models
-scales <- list(x=list(relation="free"), y=list(relation="free"))
+scales <- list(x = list(relation = "free"), y = list(relation = "free"))
 png(filename = "ML_performance_GermanCredit.png", width = 20, height = 20, units = "cm", res = 300)
-dotplot(results.GC, scales=scales, par.strip.text=list(cex=0.76), par.settings = list(par.xlab.text = list(cex = 0)))
+dotplot(results.GC, scales = scales, par.strip.text = list(cex = 0.76), par.settings = list(par.xlab.text = list(cex = 0)))
 dev.off()
 # what is the best model in terms of Balanced Accuracy"?
 
@@ -406,5 +409,5 @@ confusionMatrix(predictions.GC, GerCredTest$Class)
 ### Feature importance
 gbmImp.GC <- varImp(fit.svm_Rad.GC, scale = T)
 png(filename = "ML_FeatImp_GermanCredit.png", width = 10, height = 10, units = "cm", res = 300)
-plot(gbmImp.GC, main="German Credit Variable Importance (LDA)", top=6)
+plot(gbmImp.GC, main = "German Credit Variable Importance (LDA)", top = 6)
 dev.off()
